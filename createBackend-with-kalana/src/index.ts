94b3241d0953108405960e167d474920f726744a
savindunawanjana <savindunawanjana08@gmail.com>
2026-05-19 02:39:33 +0530

    create folder strucher and  wrote some codes

--- createBackend-with-kalana/src/index.ts
+++ createBackend-with-kalana/src/index.ts
@@ -1,6 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cros from "cors";
+dotenv.config();
+const DB_URL = process.env.MONG_URL as string;
+const PORT_NUMBER = process.env.PORT;
 
 const app = express();
-app.use(express.json())
-mongoose.connect("mongodb://localhost:27017/developersPlus").then(()=>{ })
+app.use(cros())
+app.use(express.json());
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("Mongo-Db conected... ");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+app.listen(PORT_NUMBER, () => {
+  console.log("server runing port:7000");
+});
